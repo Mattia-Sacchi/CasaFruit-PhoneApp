0f9de2543ef98f84982bc5a5cb4390bdf8c7396a
DESKTOP-O4BPLA5\Mattia Sacchi <mattiasacchi2002@gmail.com>
2023-04-25 15:57:30 +0200

    Dependencie + file picker

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -4,12 +4,13 @@ import 'Storage.dart';
 import 'package:casa_fruit/ColloWidget.dart';
 import 'package:share_plus/share_plus.dart';
 import 'dart:async';
+import 'package:file_picker/file_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title,required this.storage});
   Storage storage;
   final String title;
-
 
 
   @override
@@ -23,7 +24,7 @@ class MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-
+    getPermission();
     widget.storage.read().then((value) {
       setState(() {
         colli = value;
@@ -33,6 +34,17 @@ class MyHomePageState extends State<MyHomePage> {
     });
 
   }
+
+  Future<PermissionStatus> getPermission() async
+  {
+
+    var status = await Permission.storage.status;
+    if(status.isDenied)
+      return await Permission.storage.request();
+    return status;
+  }
+
+
 
   void addCollo(int id,String c)
   {
@@ -50,7 +62,9 @@ class MyHomePageState extends State<MyHomePage> {
     });
     addWidget(w);
     save();
+
   }
+
 
   void addWidget(ColloWidget c)
   {
@@ -66,6 +80,18 @@ class MyHomePageState extends State<MyHomePage> {
   void save()
   {
     widget.storage.write(colli);
+  }
+
+  void pickFile() async
+  {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+        allowedExtensions: ['txt'],
+        type: FileType.custom);
+    if(result != null && result.files.single.path != null)
+      {
+        PlatformFile file = result.files.first;
+        print(file.name);
+      }
   }
 
 
@@ -85,7 +111,7 @@ class MyHomePageState extends State<MyHomePage> {
           onPressed: () async {
             await Share.shareXFiles(
                 [XFile(widget.storage.localFilePath)],
-            text: "Ciao mondo");
+            text: "Condividi file");
 
           },
           tooltip: 'Share',
@@ -97,9 +123,9 @@ class MyHomePageState extends State<MyHomePage> {
           icon: Icon(Icons.add),
           color: Colors.blue,),
         IconButton(
-          onPressed: () {  },
+          onPressed: pickFile,
           tooltip: 'Get File',
-          icon: Icon(Icons.add),
+          icon: Icon(Icons.file_open),
           color: Colors.blue,),
       ],
     );
